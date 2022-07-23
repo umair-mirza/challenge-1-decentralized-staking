ad7ff2d2349ae1cdd6380816fab143c5851c5972
Umair Mirza <umair_hp@hotmail.com>
2022-07-24 02:01:59 +0500

    implemented the special receive function

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -14,7 +14,7 @@ contract Staker {
 
   event Stake(address, uint256);
 
-  uint256 public deadline = block.timestamp + 1 minutes;
+  uint256 public deadline = block.timestamp + 72 hours;
 
   mapping (address => uint256) balances;
 
@@ -25,7 +25,7 @@ contract Staker {
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
 
-  function stake() external payable {
+  function stake() public payable notCompleted {
     require(msg.value > 0, "Amount is less than or equal to zero");
     balances[msg.sender] += msg.value;
 
@@ -35,7 +35,7 @@ contract Staker {
   // After some `deadline` allow anyone to call an `execute()` function
   // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
 
-  function execute() external {
+  function execute() external notCompleted {
     require(block.timestamp >= deadline, "Deadline has not been reached yet");
     if(address(this).balance >= threshold) {
       exampleExternalContract.complete{value: address(this).balance}();
@@ -47,20 +47,30 @@ contract Staker {
   // If the `threshold` was not met, allow everyone to call a `withdraw()` function
   // Add a `withdraw()` function to let users withdraw their balance
 
-  function withdraw() external {
+  function withdraw() external notCompleted {
     require(openForWithdraw == true, "Cannot withdraw funds unless the Contract balance is less than Threshold");
     payable(msg.sender).transfer(balances[msg.sender]);
   }
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
 
-  // function timeLeft() public view returns(uint256) {
-  //   uint256 currentTime = block.timestamp;
-  //   uint256 _timeLeft = (deadline + 3 minutes) - currentTime;
-  //   return _timeLeft;
-  // }
+  function timeLeft() public view returns(uint256) {
+    if(block.timestamp < deadline) {
+      return deadline - block.timestamp;
+    } else {
+      return 0;
+    }
+  }
 
   // Add the `receive()` special function that receives eth and calls stake()
 
+  receive() external payable {
+    stake();
+  }
+
+  modifier notCompleted {
+    require(exampleExternalContract.completed() != true);
+    _;
+  }
 
 }
